74915f672c3f5711815f1923b89c6719a8fe8eac
bryce-88 <yengpeng.khor@gmail.com>
2023-02-08 10:06:05 +0800

    initial commit

---
+++ src/main/java/ibf2022/ssf/day13/model/Employee.java
@@ -0,0 +1,170 @@
+package ibf2022.ssf.day13.model;
+
+import java.util.Date;
+
+public class Employee {
+    
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String phoneNo;
+
+    private Integer salary;
+
+    private Date birthDay;
+
+    private String address;
+
+    
+
+    public Employee() {
+    }
+
+
+    public Employee(String firstName, String lastName, String email, String phoneNo, Integer salary, Date birthDay,
+            String address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNo = phoneNo;
+        this.salary = salary;
+        this.birthDay = birthDay;
+        this.address = address;
+    }
+
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    public Date getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(Date birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+    
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+        result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + ((phoneNo == null) ? 0 : phoneNo.hashCode());
+        result = prime * result + ((salary == null) ? 0 : salary.hashCode());
+        result = prime * result + ((birthDay == null) ? 0 : birthDay.hashCode());
+        result = prime * result + ((address == null) ? 0 : address.hashCode());
+        return result;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Employee other = (Employee) obj;
+        if (firstName == null) {
+            if (other.firstName != null)
+                return false;
+        } else if (!firstName.equals(other.firstName))
+            return false;
+        if (lastName == null) {
+            if (other.lastName != null)
+                return false;
+        } else if (!lastName.equals(other.lastName))
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        if (phoneNo == null) {
+            if (other.phoneNo != null)
+                return false;
+        } else if (!phoneNo.equals(other.phoneNo))
+            return false;
+        if (salary == null) {
+            if (other.salary != null)
+                return false;
+        } else if (!salary.equals(other.salary))
+            return false;
+        if (birthDay == null) {
+            if (other.birthDay != null)
+                return false;
+        } else if (!birthDay.equals(other.birthDay))
+            return false;
+        if (address == null) {
+            if (other.address != null)
+                return false;
+        } else if (!address.equals(other.address))
+            return false;
+        return true;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Employee [firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + ", phoneNo="
+                + phoneNo + ", salary=" + salary + ", birthDay=" + birthDay + ", address=" + address + "]";
+    }
+
+    
+    
+}
